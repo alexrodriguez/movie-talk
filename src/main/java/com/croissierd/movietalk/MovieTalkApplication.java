18be5541adead24de6549f31c6d27772c880e65e
Alex Rodriguez <alex.rodriguez@croissierd.com>
2018-10-10 21:03:54 -0400

    Initial commit

---
+++ src/main/java/com/croissierd/movietalk/MovieTalkApplication.java
@@ -0,0 +1,12 @@
+package com.croissierd.movietalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTalkApplication.class, args);
+	}
+}
